7866b6f42541a69d34c337cea86bffe02efe7f50
Jiří Helmich <jiri@helmich.cz>
2017-10-17 12:47:34 +0200

    fixing homepage

--- src/pages/discovery.js
+++ src/pages/discovery.js
@@ -17,15 +17,15 @@ class DiscoveryPage extends React.Component {
 
     componentDidMount() {
 
-        if (this.props.components.length === 0) {
-            const activeComponentUris = compose(
-                map(c => c.uri),
-                filter(c => c.isActive),
-                values,
-                mergeAll,
-                values,
-            )(this.props.components)
+        const activeComponentUris = compose(
+            map(c => c.uri),
+            filter(c => c.isActive),
+            values,
+            mergeAll,
+            values,
+        )(this.props.components)
 
+        if (activeComponentUris.length === 0) {
             this.props.handleDiscoveryStart(activeComponentUris)
         } else {
             this.props.handleDiscoveryStartWithInput(this.props.inputUri)
