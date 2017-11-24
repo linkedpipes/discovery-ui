6f214be3457510d5412d150b69265045698042e3
Jirka Helmich <jiri@helmich.cz>
2017-11-24 08:04:09 +0100

    Fixes layout margin.

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,23 +2,34 @@ import React from 'react'
 import Head from 'next/head'
 import NavBar from '../components/navBar'
 
-export default ({ children, title = 'Discovery UI' }) => (
-    <div>
-        <Head>
-            <title>{ title }</title>
-            <meta charSet='utf-8' />
-            <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-            <link rel='stylesheet' href='/static/react-md.light_blue-amber.min.css' />
-            <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto:300,400,500' />
-            <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Material+Icons' />
-        </Head>
-        <div>
-            <NavBar />
-            <div style={{ width: '75%', margin: 'auto', paddingTop: '20px' }}>
-                { children }
+class Layout extends React.Component {
+
+    componentWillMount() {
+        document.body.style.margin = 0;
+    }
+
+    render () {
+        return (
+            <div>
+                <Head>
+                    <title>Discovery Runner</title>
+                    <meta charSet='utf-8' />
+                    <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+                    <link rel='stylesheet' href='/static/react-md.light_blue-amber.min.css' />
+                    <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto:300,400,500' />
+                    <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Material+Icons' />
+                </Head>
+                <div>
+                    <NavBar />
+                    <div style={{ width: '75%', margin: 'auto', paddingTop: '20px' }}>
+                        { this.props.children }
+                    </div>
+                    <footer>
+                    </footer>
+                </div>
             </div>
-            <footer>
-            </footer>
-        </div>
-    </div>
-)
+        )
+    }
+}
+
+export default Layout
