4c41147f51b4976df278ca65a5d3ab86e90958eb
Jirka Helmich <jiri@helmich.cz>
2017-11-24 08:28:40 +0100

    Fixes header.

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,34 +2,23 @@ import React from 'react'
 import Head from 'next/head'
 import NavBar from '../components/navBar'
 
-class Layout extends React.Component {
-
-    componentWillMount() {
-        document.body.style.margin = 0;
-    }
-
-    render () {
-        return (
-            <div>
-                <Head>
-                    <title>Discovery Runner</title>
-                    <meta charSet='utf-8' />
-                    <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-                    <link rel='stylesheet' href='/static/react-md.light_blue-amber.min.css' />
-                    <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto:300,400,500' />
-                    <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Material+Icons' />
-                </Head>
-                <div>
-                    <NavBar />
-                    <div style={{ width: '75%', margin: 'auto', paddingTop: '20px' }}>
-                        { this.props.children }
-                    </div>
-                    <footer>
-                    </footer>
-                </div>
+export default ({ children, title = 'Discovery UI' }) => (
+    <div>
+        <Head>
+            <title>{ title }</title>
+            <meta charSet='utf-8' />
+            <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+            <link rel='stylesheet' href='/static/react-md.light_blue-amber.min.css' />
+            <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto:300,400,500' />
+            <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Material+Icons' />
+        </Head>
+        <div>
+            <NavBar />
+            <div style={{ width: '75%', margin: 'auto', paddingTop: '80px' }}>
+                { children }
             </div>
-        )
-    }
-}
-
-export default Layout
+            <footer>
+            </footer>
+        </div>
+    </div>
+)
--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,15 +1,24 @@
 import React from 'react'
 import Toolbar from 'react-md/lib/Toolbars'
+import Button from 'react-md/lib/Buttons'
 
 const titleLink = <a href="/">LinkedPipes Discovery</a>
 
 const NavBar = ({ children }) => (
     <Toolbar
+        fixed
         colored
         title={titleLink}
-    >
-        {children}
-    </Toolbar>
+        actions={(
+            <a href="/input">
+                <Button
+                    raised
+                    label='Advanced mode'
+                    secondary
+                />
+            </a>
+        )}
+    />
 );
 
 export default NavBar
